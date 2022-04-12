ce2ce7bc40b3f96c3ec004a7456990742a1292cb
znjs <jasrikar@gmail.com>
2022-04-12 19:21:42 +0530

    fix: lower chars crossing barrier by num

--- question3.js
+++ question3.js
@@ -4,20 +4,23 @@ const encodeString = (str, num) => {
   num = num % 26;
   let charArr = str.split("");
   charArr = charArr.map((char) => {
-    if (
-      (char.charCodeAt(0) + num >= 65 && char.charCodeAt(0) + num <= 90) ||
-      (char.charCodeAt(0) + num >= 97 && char.charCodeAt(0) + num <= 122)
-    ) {
+    if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
+      if (char.charCodeAt(0) + num > 90) {
+        return char.charCodeAt(0) + num - 26;
+      }
+      return char.charCodeAt(0) + num;
+    } else if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
+      if (char.charCodeAt(0) + num > 122) {
+        return char.charCodeAt(0) + num - 26;
+      }
       return char.charCodeAt(0) + num;
     }
-    return char.charCodeAt(0) + num - 26;
   });
   charArr = charArr.map((charCode) => String.fromCharCode(charCode));
   return charArr.join("");
 };
 
-console.log(encodeString("neogCamp", 3000));
-
+console.log(encodeString("Shobit", 23));
 //Char Code for edge cases
 //A => 65
 //a => 97
