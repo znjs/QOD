60d0d2e97cd3c26e53781d743ae54860545b7569
znjs <jasrikar@gmail.com>
2022-04-13 15:01:35 +0530

    fix: varible declaration

--- question6.js
+++ question6.js
@@ -1,7 +1,7 @@
 // *QUESTION: Write a function which takes and array and a number and returns the index of the number in the array
 
 const indexOf = (arr, num) => {
-  for (i in arr) {
+  for (let i in arr) {
     if (arr[i] === num) return i;
   }
   return -1;
