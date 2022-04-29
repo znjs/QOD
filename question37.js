4f1a9c4c595dba645e9d04dad8341fdac2b1e071
znjs <jasrikar@gmail.com>
2022-04-29 13:39:01 +0530

    Q37

---
+++ question37.js
@@ -0,0 +1,22 @@
+// *QUESTION: Given a sting s, find the first non-repeating character in it and return its index if it doesn't exist, return -1
+
+const firstNonRepeatingChar = (s) => {
+  let charTracker = [];
+  let removeChar = [];
+  for (let i of s) {
+    if (charTracker.includes(i)) {
+      removeChar.push(i);
+    } else charTracker.push(i);
+  }
+  removeChar = Array.from(new Set(removeChar));
+
+  for (let i of removeChar) {
+    charTracker.splice(charTracker.indexOf(i), 1);
+  }
+  console.log(charTracker);
+  console.log(removeChar);
+  return charTracker.length > 0 ? charTracker[0] : -1;
+};
+
+console.log(firstNonRepeatingChar("nnneeooggccaammpp"));
+console.log(firstNonRepeatingChar("neogcamp"));
