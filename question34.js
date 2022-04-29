382587213cf752d4ba0180f0e0c6f5c25a08b218
znjs <jasrikar@gmail.com>
2022-04-29 11:01:06 +0530

    fix: Q34

--- question34.js
+++ question34.js
@@ -4,14 +4,20 @@ const mostOccurredNumber = (arr) => {
     frequencyObject[i] = (frequencyObject[i] || 0) + 1;
   }
   let values = Object.values(frequencyObject).sort((a, b) => a - b);
+
   values = values[values.length - 1];
   let keyArr = [];
+
   for (let i in frequencyObject) {
     if (frequencyObject[i] === values) {
       keyArr.push(i);
     }
   }
-  return keyArr.sort((a, b) => b - a)[0];
+  let max = -Infinity;
+  for (i of keyArr) {
+    if (max < i) max = i;
+  }
+  return max;
 };
 
 console.log(mostOccurredNumber([1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 7, 8, 8, 8, 8]));
