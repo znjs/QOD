8b3737b03f35329975617221d964a3de0c220af8
znjs <jasrikar@gmail.com>
2023-09-27 00:21:10 +0530

    added 2 questions

---
+++ questions/question55.js
@@ -0,0 +1,29 @@
+/**
+ * 1512. Number of Good Pairs
+ * Source: https://leetcode.com/problems/number-of-good-pairs/
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+let numIdenticalPairs = function (nums) {
+  let result = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) result++;
+    }
+  }
+  return result;
+};
