a1a9fcc06e4af5616067b8d53ff8c0f030234a8d
znjs <jasrikar@gmail.com>
2023-10-13 23:36:03 +0530

    config: added

---
+++ arrays/prob1.js
@@ -0,0 +1,7 @@
+/**
+ * Binary Search
+ * Given a sorted array of integers, find the index of a target value using a binary search algorithm.
+ * A binary search finds an item in a sorted array by repeatedly choosing a middle value and dividing the search interval in half.
+ * @param {number[]} arr
+ * @returns {number}
+ */
